ff39de0055275f04763a229c3c461e52aba41376
julio <js8215001@gmail.com>
2023-11-09 20:55:34 -0600

    campo faltante agregado

--- aseguradora-back/src/database/consultas/Reparaciones.js
+++ aseguradora-back/src/database/consultas/Reparaciones.js
@@ -9,9 +9,9 @@ export const queriesReparaciones = (orderBy, ordenar) => {
         getAll: `SELECT * FROM ${tablename}`,
         createReparacion: `INSERT INTO ${tablename} ([modelo],[placa],[fechaIngreso],[detalles],[imagen],[idCliente],[idMarca],[idTaller],[color],[estado]) VALUES
             (@modelo,@placa,@fechaIngreso,@detalles,@imagen,@idCliente,@idMarca,@idTaller,@color,'activo')`,
-        getActivos: `SELECT * FROM ${tablename} where estado = 'activo'`,
-        getEnProceso: `SELECT * FROM ${tablename} where estado = 'proceso'`,
-        getFinalizados: `SELECT * FROM ${tablename} where estado = 'finalizado'`,
-        getRechazado: `SELECT * FROM ${tablename} where estado = 'rechazado'`,
+        getActivos: `SELECT ${tablename}.*, (SELECT NombreCliente FROM Clientes where idCliente = ${tablename}.idCliente) as cliente FROM ${tablename} where estado = 'activo'`,
+        getEnProceso: `SELECT ${tablename}.*, (SELECT NombreCliente FROM Clientes where idCliente = ${tablename}.idCliente) as cliente FROM ${tablename} where estado = 'proceso'`,
+        getFinalizados: `SELECT ${tablename}.*, (SELECT NombreCliente FROM Clientes where idCliente = ${tablename}.idCliente) as cliente ${tablename} where estado = 'finalizado'`,
+        getRechazado: `SELECT ${tablename}.*, (SELECT NombreCliente FROM Clientes where idCliente = ${tablename}.idCliente) as cliente ${tablename} where estado = 'rechazado'`,
     };
 };
